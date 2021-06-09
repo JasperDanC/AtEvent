2a8c952b40be4acecbeb3f8a2d0000901b1342bd
JasperDanC <jdc498@mail.usask.ca>
2021-06-09 16:28:11 -0600

    finished up empty calendar screens

--- at_event/lib/screens/calendar_screen.dart
+++ at_event/lib/screens/calendar_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:at_event/constants.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 void main() {
   runApp(CalendarScreen());
 }
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  final CalendarController _controller = CalendarController();
+  int switchIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,18 +31,62 @@ class CalendarScreen extends StatelessWidget {
                 vertical: 55,
               ),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Your Calendar",
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Your Calendar",
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      MaterialButton(
+                          shape: CircleBorder(),
+
+                          onPressed: (){},
+                          child: Icon(
+                            Icons.chevron_left,
+                            color: Colors.white,
+                            size: 50.0,
+                          )
+                      )
+                    ],
+                  ),
+                  ToggleSwitch(
+                    totalSwitches: 3,
+                    labels: ["Month", "Week", "Day"],
+                    initialLabelIndex: switchIndex,
+                    onToggle: (index) {
+                      setState(() {
+                        switchIndex = index;
+                        switch (index) {
+                          case 0:
+                            {
+                              _controller.view = CalendarView.month;
+                            }
+                            break;
+                          case 1:
+                            {
+                              _controller.view = CalendarView.week;
+                            }
+                            break;
+                          case 2:
+                            {
+                              _controller.view = CalendarView.day;
+                            }
+                            break;
+                        }
+                      });
+                    },
                   ),
                   Expanded(
                     child: SfCalendar(
+                      controller: _controller,
                       monthCellBuilder: (context, details) {
                         bool isTopLeft =
                             details.visibleDates.first == details.date;
@@ -76,7 +129,6 @@ class CalendarScreen extends StatelessWidget {
                         DateTime now = DateTime.now();
                         DateTime today = DateTime(now.year, now.month, now.day);
 
-
                         return Container(
                           decoration: BoxDecoration(
                             color: details.date == today
@@ -103,6 +155,7 @@ class CalendarScreen extends StatelessWidget {
                           ),
                         );
                       },
+
                       headerStyle: CalendarHeaderStyle(
                           textStyle: TextStyle(
                               fontWeight: FontWeight.bold, color: Colors.white),
@@ -112,7 +165,8 @@ class CalendarScreen extends StatelessWidget {
                               fontWeight: FontWeight.bold, color: Colors.white),
                           backgroundColor: kPrimaryBlue),
                       view: CalendarView.month,
-                      backgroundColor: Colors.transparent,
+                      backgroundColor: _controller.view == CalendarView.month? Colors.transparent: Colors.white,
+                      cellBorderColor: _controller.view == CalendarView.month? Colors.white: Colors.black,
                     ),
                   )
                 ],
