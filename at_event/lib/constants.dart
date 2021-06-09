b28132910ec767f9f5c2751b862f81b80c164df9
JasperDanC <jdc498@mail.usask.ca>
2021-06-09 13:12:50 -0600

    Added Colour constants

---
+++ at_event/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundGrey = Color(0xFF555555);
+const Color kForegroundGrey = Color(0xFF404040);
+const Color kPrimaryBlue = Color(0xFF3F51B5);
+const Color kEventBlue = Color(0xFF0D9DAD);
