a17daa74cd13e2132e27e690904158500e751e05
JasperDanC <jdc498@mail.usask.ca>
2021-06-10 11:30:41 -0600

    remove appbar from main

--- at_event/lib/main.dart
+++ at_event/lib/main.dart
@@ -12,25 +12,8 @@ class Vento extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
           primaryColor: kForegroundGrey,
           scaffoldBackgroundColor: kBackgroundGrey),
-      home: Scaffold(
-        appBar: AppBar(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(45.0),
-            bottomLeft: Radius.circular(45.0),
-          )),
-          centerTitle: true,
-          backgroundColor: kPrimaryBlue,
-          title: Text(
-            "@Vento",
-            style: TextStyle(
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white),
-          ),
-        ),
-        body: WelcomeScreen(),
-      ),
+      home: WelcomeScreen(),
+
     );
   }
 }
