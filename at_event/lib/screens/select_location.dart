1b972a03121e23653de17b4070b6b6a2401cb458
JesseAguirre <jesseadrian@hotmail.com>
2021-06-23 14:04:12 -0600

    Started Working on Location Screen

--- at_event/lib/screens/select_location.dart
+++ at_event/lib/screens/select_location.dart
@@ -1,3 +1,84 @@
 import 'package:at_common_flutter/services/size_config.dart';
 import 'package:at_common_flutter/widgets/custom_input_field.dart';
 import 'package:at_event/Widgets/custom_toast.dart';
+import 'package:at_event/Widgets/location_tile.dart';
+import 'package:at_event/screens/background.dart';
+import 'package:at_event/utils/constants.dart';
+import 'package:at_location_flutter/at_location_flutter.dart';
+import 'package:at_location_flutter/location_modal/location_modal.dart';
+import 'package:at_location_flutter/service/my_location.dart';
+import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
+
+class SelectLocation extends StatefulWidget {
+  @override
+  _SelectLocationState createState() => _SelectLocationState();
+}
+
+class _SelectLocationState extends State<SelectLocation> {
+  String inputText = '';
+  bool isLoader = false;
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Container(
+      height: SizeConfig().screenHeight * 0.8,
+      padding: EdgeInsets.fromLTRB(28.toWidth, 20.toHeight, 17.toWidth, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: CustomInputField(
+                  hintText: 'Search for a Location...',
+                  initialValue: inputText,
+                  onSubmitted: (String str) async {
+                    setState(() {
+                      isLoader = true;
+                    });
+                    await SearchLocationService().getAddressLatLng(str);
+                    setState(() {
+                      isLoader = false;
+                    });
+                  },
+                  value: (val) {
+                    inputText = val;
+                  },
+                  icon: Icons.search,
+                  onIconTap: () async {
+                    setState(() {
+                      isLoader = true;
+                    });
+                    await SearchLocationService().getAddressLatLng(inputText);
+                    setState(() {
+                      isLoader = false;
+                    });
+                  },
+                ),
+              ),
+              SizedBox(width: 10.toWidth),
+              InkWell(
+                onTap: () => Navigator.pop(context),
+                child: Text('Cancel',
+                    style: kNormalTextStyle.copyWith(fontSize: 16)),
+              ),
+            ],
+          ),
+          SizedBox(height: 20.toHeight),
+          Divider(),
+          SizedBox(height: 18.toHeight),
+          InkWell(
+            onTap: () async {
+              var point = await getMyLocation();
+              if (point == null) {
+                CustomToast().show('Unable to access location', context);
+                return;
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
