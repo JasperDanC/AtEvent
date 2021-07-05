aa17be7c4f494f37e777103c2f259c26cb65db4d
JasperDanC <jdc498@mail.usask.ca>
2021-07-05 15:41:05 -0600

    began working on group implementation and deleting invitations is close to working

---
+++ at_event/lib/models/group_model.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+class GroupModel {
+  GroupModel();
+  String title;
+  String description;
+  String imageURL;
+  List<String> eventKeys;
+  List<String> atSignMembers;
+  List<String> invitees;
+  String atSignCreator;
+  String key;
+
+  GroupModel.fromJson(Map<String, dynamic> data) {
+    title = data['title'] ?? '';
+    key = data['key'] ?? '';
+    imageURL = data['imageURL'] ?? '';
+    description = data['description'] ?? '';
+    eventKeys = data['eventKeys'] == '[]' || data['eventKeys'] == ''
+        ? []
+        : data['eventKeys'].split(',') ?? [];
+    atSignMembers = data['atSignMembers'] == '[]' || data['atSignMembers'] == ''
+        ? []
+        : data['atSignMembers'].split(',') ?? [];
+    invitees = data['invitees'] == '[]' || data['invitees'] == ''
+        ? []
+        : data['invitees'].split(',') ?? [];
+    atSignCreator = data['atSignCreator' ?? ''];
+  }
+
+  static String convertGroupToJson(GroupModel group) {
+    var groupJson = json.encode({
+      'title': group.title != null ? group.title.toString() : '',
+      'imageURL': group.imageURL != null ? group.imageURL.toString() : '',
+      'description': group.description,
+      'atSignCreator': group.atSignCreator.toString(),
+      'invitees': group.invitees.length > 0 ? group.invitees.join(',') : '[]',
+      'atSignMembers':
+          group.atSignMembers.length > 0 ? group.atSignMembers.join(',') : '[]',
+      'eventKeys':
+          group.eventKeys.length > 0 ? group.eventKeys.join(',') : '[]',
+      'key': '${group.key}',
+    });
+    return groupJson;
+  }
+}
